276acf09a6cc6e8a6c1fc851e6a9c290fa3b45e8
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2022-11-27 01:52:10 -0800

    update comments

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -8,16 +8,16 @@ arrayName.append("reza")
 arrayName.append("akbar")
  
 let arrayNameUnmutable = [myName,yourName]
-//todo - you can not change let variable
+//you can not change let variables
 //arrayNameUnmutable.append("newValue")
 
 
-//todo - you can change a let variable by asgin a refrence types
+//you can change a let variable by asgin a refrence types
 let oldArray = NSMutableArray(
     array: ["foo","zee"]
 )
-//todo - change a unmutable varibale by asign a refrence types
+//change a unmutable varibale by asign a refrence types
 oldArray.add("jee")
 var newArray=oldArray
 newArray.add("lee")
-//todo - you can see the oldArray and NewArray are the same
+//you can see the oldArray and NewArray are the same
