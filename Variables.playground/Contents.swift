035c84e6b82e8f88d0940020f56ee23f5a69bacf
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2022-11-27 00:05:43 -0800

    variables

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,3 +1,23 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, srfsfs"
+let myName = "milad"
+let yourName = "milad"
+
+var arrayName = [myName,yourName]
+arrayName.append("reza")
+arrayName.append("akbar")
+ 
+let arrayNameUnmutable = [myName,yourName]
+//todo - you can not change let variable
+//arrayNameUnmutable.append("newValue")
+
+
+//todo - you can change a let variable by asgin a refrence of class
+let oldArray = NSMutableArray(
+    array: ["foo","zee"]
+)
+//todo - change a unmutable varibale by asign a refrence of class
+oldArray.add("jee")
+var newArray=oldArray
+newArray.add("lee")
+//todo - you can see the oldArray and NewArray are the same 
