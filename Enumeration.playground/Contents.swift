89a376fc7b000bd62f5f7e442e082c0fbbf4856b
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2022-12-01 00:39:51 -0800

    add emu

--- Enumeration.playground/Contents.swift
+++ Enumeration.playground/Contents.swift
@@ -1,3 +1,28 @@
-import UIKit
+import Foundation
+ 
+enum Animals{
+    case butteerfly
+    case dog,cat
+    case cow,rabbit,fox
+}
 
-var greeting = "Hello, playground"
+let animal=Animals.cat
+switch animal {
+case .cat:
+    "this is a cat"
+break
+case .dog:
+    "this is a dog"
+    break
+case .butteerfly:
+    "this is a butteerfly"
+    //you can cover other situation with default key word
+default:
+    "this is some thing else"
+//case .cow:
+//    "this is a cow"
+//case .rabbit:
+//    "this is a rabbit"
+//case .fox:
+//    "this is a fox"
+}
