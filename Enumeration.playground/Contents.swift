dace59759fc73137aac3859e4b760de531deef2a
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2022-12-02 22:44:18 -0800

    compare switch enum values

--- Enumeration.playground/Contents.swift
+++ Enumeration.playground/Contents.swift
@@ -16,7 +16,7 @@ case .dog:
     break
 case .butteerfly:
     "this is a butteerfly"
-    //you can cover other situation with default key word
+    //1- you can cover other situation with default key word
 default:
     "this is some thing else"
 //case .cow:
@@ -26,3 +26,43 @@ default:
 //case .fox:
 //    "this is a fox"
 }
+
+
+enum Shortcuts{
+    case fileAndFolder(path: URL,name :String)
+    case wwwUrl(path : URL)
+    case song(artist :String,songName: String)
+}
+
+let wwwApple=Shortcuts.wwwUrl(path: URL(string: "www.apple.com")!)
+//you cannot compare a wwwApple with a url you pass to enum beacuse you have to implement == in enum
+
+switch wwwApple{
+case .fileAndFolder(path: let path, name: let name):
+    print("\(path) - \(name)")
+case .wwwUrl(path: let path):
+    print("\(path) ")
+case .song(artist: let artist, songName: let songName):
+    print("\(artist) - \(songName)")
+}
+
+//you can make a summary of (( let )) after ((case)) key word
+switch wwwApple{
+case let.fileAndFolder(path: path, name: name):
+    print("\(path) - \(name)")
+case let.wwwUrl(path: path):
+    print("\(path) ")
+case let.song(artist: artist, songName: songName):
+    print("\(artist) - \(songName)")
+}
+
+//you can remove external argument
+switch wwwApple{
+case let.fileAndFolder( path, name):
+    print("\(path) - \(name)")
+case let.wwwUrl( path):
+    print("\(path) ")
+case let.song(artist, songName):
+    print("\(artist) - \(songName)")
+}
+
