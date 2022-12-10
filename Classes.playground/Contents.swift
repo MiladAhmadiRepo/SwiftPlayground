15c9f1853c6b1ca13b56991021fb50df04b34400
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2022-12-10 09:37:34 -0800

    complete class

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -14,10 +14,11 @@ class Person {
     }
 }
 
-// you can see we asign new value to let variable without any error
-// why, beacusse all classes in swift are by refrence types and we can change them iternaly
+
 let foo = Person(name: "foo", age: 20)
 foo.age
+// you can see we asign new value to let variable without any error ((let foo))
+// why? beacusse all classes in Swift are ((in refrence types)) and we can change them iternally
 foo.increaseAge()
 foo.age
 
@@ -57,3 +58,79 @@ person2.age
 //person2.age+=1
 
 
+class Tesla{
+    let manufacturer = "Tesla"
+    let model :String
+    let year : Int
+    
+    //designated initilazer could be implemet multi time with multi shape
+    init() {
+        model="x"
+        year=2023
+    }
+    
+    init(model:String,year:Int) {
+        self.model = model
+        self.year = year
+    }
+    //designated initilazer could not call in another designated initalizer
+    //  init() {
+    //      self.init(model : "x" , year : 2023)
+    //  }
+    //Designated initializer cannot delegate (with 'self.init'); it should be a convenience initializer
+ 
+    
+    convenience init (model : String)
+    {
+        //you can see ((convenience initializer)) delegated ((Designated initializer))
+        self.init(model : model , year: 2023)
+    }
+    
+}
+
+class TeslaModelY : Tesla{
+    //for ((init )) we have to override ((init)) in super class
+    override init(model: String, year: Int) {
+        super.init(model: model, year: year)
+    }
+    
+    //you can not call ((convenience initializer)) in ((Designated initializer)) for example in child init method
+    //override init(model: String, year: Int) {
+        //super.init(model: model)
+    //}
+    
+}
+
+
+//classes are ((refrences type))
+let fooBar = Person2(age: 20)
+fooBar.age
+func doSomeThing(with person : Person2)
+{
+    person.increaseAge()
+}
+doSomeThing(with : fooBar)
+fooBar.age
+
+
+class MyClass{
+    init (){
+        print("init call" )
+    }
+    func doSomeThing(){
+        print("do some thing")
+    }
+    //deinit would be call after the object of class remove in memory
+    
+    deinit{
+        print("deinit call")
+    }
+}
+
+let myclass=MyClass()
+
+let myClosure = {
+    let myclass2 = MyClass()
+    myclass2.doSomeThing()
+}
+myClosure()
