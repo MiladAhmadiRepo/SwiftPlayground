2fed6a3992777c1e97fec7b7220d0a7e493b16bb
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2022-12-13 10:38:29 -0800

    complete protocols

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -50,3 +50,90 @@ let wiskers = Cat()
 wiskers.jumping()
 
 
+//you can add properties to protocols like this
+protocol HasName{
+    //this is the way to write down ((get properties)) in protocols
+    var name : String { get }
+    //you can not write variable with ((let)) in protocols
+    //let familyName : String { get }
+    //in the right way you can use ((var)) with ((get))
+    var familyName : String { get }
+    var age : Int { get set }
+}
+
+struct Dog : HasName{
+    //you can comform  the properties in child struct or class like this
+    //var name: String
+    //the name variable is a (( get )) property and you can set it as a  (( let )) variable
+    let name: String
+    var familyName: String
+    var age: Int
+}
+let woof = Dog(name: "woof", familyName: "woofy", age: 10)
+woof.name
+woof.age
+//woof.age += 1
+
+
+//we add properties to protocols to use in extention and child classes
+protocol Vehicle{
+    var speed : Int {get set}
+    mutating func increaseSpeed(by value :Int)
+}
+
+extension Vehicle {
+    mutating func increaseSpeed(by value :Int){
+        self.speed += value
+    }
+}
+
+struct Bike :Vehicle{
+    var speed: Int
+    init(){
+        self.speed = 0
+    }
+}
+
+var bike = Bike()
+bike.speed
+bike.increaseSpeed(by: 10)
+bike.speed
+
+//to comform a object is a protocol you can use ((is)) key word
+func describe (obj:Any)
+{
+    if(obj is Vehicle)
+    {
+        print("object comforms to the Vehicle protocols ")
+    }
+    else
+    {
+        print("object doesnot comform to the Vehicle protocols ")
+    }
+}
+
+describe(obj: bike)
+struct Worm{
+    
+}
+describe(obj: Worm())
+
+
+//you can use the ((as?)) key word to tell to the Swift the type of object
+
+func increaseSpeedIfVehicle(obj : Any)
+{
+    if var vehicle = obj as? Vehicle {
+        vehicle.speed
+        vehicle.increaseSpeed(by: 15)
+        vehicle.speed
+    }
+    else
+    {
+        print("this was not a vehicle")
+    }
+}
+
+increaseSpeedIfVehicle(obj: bike)
+// you can see the bike.speed not changed beacuase that is a ((struct)) and swift copy that ((by value))
+bike.speed
