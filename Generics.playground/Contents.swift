796e03e10c6b0b3e4a270c654838f61cd3d6d29b
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2022-12-17 21:04:16 -0800

    complete generics

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,4 +1,5 @@
 import Foundation
+import os
 
 
 //generics it used to avoid writing the same function multiple times
@@ -32,7 +33,8 @@ func performNumericGeneric2 <N>(_ op: (N , N) -> N, on lhs : N, other rhs : N) -
     op(lhs , rhs)
 }
 // look at the ((where)) section , we define type of  N
-//----------------------------------------------------
+
+//---------------------------------------------------------------------------
 
 protocol CanRun{
     func run()
@@ -64,4 +66,118 @@ func canRunAndJump<T : CanRun & CanJump>(value : T){
 let person = Person()
 canRunAndJump(value: person)
 
-//----------------------------------------------------------------
+//---------------------------------------------------------------------------
+
+//there are many type of declaring array of string
+let names = ["foo","bar"]
+let names2 : [String] = ["foo","bar"]
+let names3 : Array<String> = ["foo","bar"]
+//you can see the array of string is a generic type like this ((Array<Element>))
+//we can extend generic type like 2 ways
+//extension Array where Element == String
+//extension [String]
+extension Array where Element == String {
+    func longestString() -> String? {
+        self.sorted { (lhs: String, rhs: String) -> Bool in
+            return lhs.count > rhs.count
+        }.first
+    }
+}
+
+["baz","baz foo","foo"].longestString()
+
+//---------------------------------------------------------------------------
+
+//((associate types)) in protocols make generic protocols
+protocol View {
+    func addSubView (_ view : View)
+}
+//now add extention to implement the addSubView function
+extension View{
+    func addSubView (_ view : View){
+            //empty
+    }
+}
+
+//now we create a structure which inherited view protocol
+struct Button :View{
+    //empty
+}
+
+struct Table : View{
+    
+}
+//we wanna represent any object in system that can be change to view
+//we create a new protocol
+protocol presentableAsView {
+    //we tell to swift this protocol use generic type by ((associatetype))
+    associatedtype ViewType :View
+    
+    //now we create a functio to produce a TypeView ((if the type would be a buttom ))
+    //this function produce a button ((if the type would be a table ))
+    //this function produce a table
+    func produceView () -> ViewType
+   
+    func configure( superView : View , thisView : ViewType )
+    func present ( view : ViewType , on superView : View)
+}
+
+
+extension presentableAsView {
+    func configure( superView : View , thisView : ViewType ){
+        //empty  by default
+    }
+    func present ( view : ViewType , on superView : View)
+    {
+        superView.addSubView(view)
+    }
+    
+}
+
+struct MyButton : presentableAsView{
+    func produceView() -> Button {
+        Button()
+    }
+    //you can see when you change return value of ((produceView)) method  to ((Button ))
+    //swift change any viewType to Button
+    func configure(superView: View, thisView: Button) {
+        //empty
+    }
+}
+
+//---------------------------------------------------------------
+
+//you can constrain your extention to a specific types
+
+extension presentableAsView where ViewType == Button{
+    func doSomeThingWithButton(){
+        print("this is a button")
+    }
+}
+let buttom : MyButton()
+//you can see we create an instance of MyButton and we access to  ((doSomeThingWithButton)) function
+buttom.doSomeThingWithButton()
+
+
+struct MyTable : presentableAsView{
+    func produceView() ->   Table{
+        Table()
+    }
+}
+//on the other hand if we create an instance of MyTable 
+//we cannot access to ((doSomeThingWithButton)) function
+//because we set constrain for extention
+let table = MyTable()
+//table.doSomeThingWithButton()
+
+//---------------------------------------------------------------
+
+//there si another example of generic extention on array in swift
+extention [Int]
+{
+    func average() -> Double {
+        return Double(self.reduce(0,+)) / Double(self.count())
+    }
+}
+
+[1,5,7,9,3].average()
